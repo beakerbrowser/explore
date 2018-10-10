593753dab7af2818837dc0390af4f2424244b378
Paul Frazee <pfrazee@gmail.com>
2018-10-10 16:21:24 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+var $ = document.querySelector.bind(document)
+var $$ = document.querySelectorAll.bind(document)
+
+// events
+// =
+
+window.addEventListener('load', function () {
+  setCategory(location.hash || 'whats-new')
+  if (!('DatArchive' in window)) {
+    document.body.classList.add('nodat')
+  }
+})
+
+window.addEventListener('hashchange', function () {
+  setCategory(location.hash)
+})
+
+function setCategory (v) {
+  currentCategory = v
+  if (currentCategory.charAt(0) === '#') {
+    currentCategory = currentCategory.slice(1)
+  }
+
+  try { $('nav a.current').classList.remove('current') } catch (e) {}
+  $('nav a[href="#'+currentCategory+'"]').classList.add('current')
+
+  try { $('.content.current').classList.remove('current') } catch (e) {}
+  $('#content-' + currentCategory).classList.add('current')
+}
